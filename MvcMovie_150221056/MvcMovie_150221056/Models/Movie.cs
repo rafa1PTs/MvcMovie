248ab1f93aa08333b84f3b22ad2ee1e6f720dc92
rafa1PTs <150221056@estudantes.ips.pt>
2017-10-29 01:18:32 +0100

    Controller methods and views

--- MvcMovie_150221056/MvcMovie_150221056/Models/Movie.cs
+++ MvcMovie_150221056/MvcMovie_150221056/Models/Movie.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie_150221056.Models
@@ -10,7 +7,9 @@ namespace MvcMovie_150221056.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
